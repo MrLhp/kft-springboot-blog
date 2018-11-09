b672dd09164e0bd2876f1e4ce8e344c2e706a231
haipei-laptop <happy612@vip.qq.com>
2018-11-09 22:10:04 +0800

    添加等额本息，等额本金计算方式

---
+++ src/main/java/com/kafeitoo/blog/bean/EqualAmountBean.java
@@ -0,0 +1,48 @@
+package com.kafeitoo.blog.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 分期结果
+ */
+@Getter
+@Setter
+public class EqualAmountBean {
+    /**
+     * 序号
+     */
+    int no;
+    /**
+     * 周期
+     */
+    int days;
+    /**
+     * 还款日
+     */
+    Date repaymentDate;
+    /**
+     * 还款额（x100）
+     */
+    long repaymentMoney;
+    /**
+     * 本金（x100）
+     */
+    long principal;
+    /**
+     * 利息（x100）
+     */
+    long interest;
+    /**
+     * 剩下待还（x100）
+     */
+    long amountMoney;
+
+    public EqualAmountBean() {
+    }
+
+}
---
+++ src/main/java/com/kafeitoo/blog/model/money/InDebt.java
@@ -0,0 +1,19 @@
+package com.kafeitoo.blog.model.money;
+
+import com.kafeitoo.blog.common.AbstractAuditModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * 欠款
+ */
+@Getter
+@Setter
+@Entity
+public class InDebt extends AbstractAuditModel {
+
+
+}
